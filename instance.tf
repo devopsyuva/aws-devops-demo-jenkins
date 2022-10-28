7fbba75bf1975f1a36d43a771a65b563c17446fa
sudheer reddy duba <info@sudheerdevops.com>
2022-10-28 13:20:56 +0530

    Update key-pair for instance

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami                         = "ami-09e67e426f25ce0d7" #Ubuntu 20.04 LTS Free Tier Image
   instance_type               = "t3.micro"
-  key_name                    = "sudhams_virginia_demo"
+  key_name                    = "awsdemokey"
   subnet_id                   = aws_subnet.public_subnet.id
   vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
   user_data                   = file("nginx-install.sh")
